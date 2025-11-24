9ed4e5f99c8c194a640b54da450ee3e004de2eb9
Andrew Morgan <andrewdagamer741@gmail.com>
2025-11-23 18:49:13 -0700

    Emulator!
    Added an emulator that runs after compilation. I was able to get it to clock all the way up to ~0.22ghz on a 3.2ghz computer (about 13-16 native instructions per emulated instruction), which is way more overkill than necessary.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 
 fn get_macros(scripts: &mut Vec<Vec<String>>, global_macros: &mut Vec<(String, Vec<String>, Vec<Vec<String>>)>) -> Vec<(String, Vec<String>, Vec<Vec<String>>)> {
     let mut macros = vec![];
@@ -63,6 +65,7 @@ static OP_CODES: &[(u8, usize, [usize; 3], &str)] = &[
     (0b000_00010, 0, [0, 0, 0], "SetDspInY"),
     (0b000_00011, 0, [0, 0, 0], "SetDspInCol"),
     (0b000_00100, 2, [1, 0, 0], "Plot"),
+    (0b000_00101, 0, [0, 0, 0], "Kill"),
     (0b000_00110, 0, [0, 0, 0], "SetPtr"),
     (0b000_00111, 0, [0, 0, 0], "PgcL"),
     (0b000_01000, 0, [0, 0, 0], "PgcR"),
@@ -90,6 +93,7 @@ static OP_CODES: &[(u8, usize, [usize; 3], &str)] = &[
     (0b010_00100, 0, [0, 0, 0], "SetC"),
     (0b010_00101, 0, [0, 0, 0], "RsetC"),
     (0b010_00110, 0, [0, 0, 0], "Zero"),
+    (0b010_00111, 0, [0, 0, 0], "RsetO"),
     (0b011_00000, 1, [0, 0, 0], "LodL"),
     (0b011_00001, 1, [0, 0, 0], "LodR"),
     (0b011_00010, 1, [0, 0, 0], "WrtO"),
@@ -111,17 +115,17 @@ static OP_CODES: &[(u8, usize, [usize; 3], &str)] = &[
     (0b101_00100, 0, [0, 0, 0], "DPtrR"),
     (0b101_00101, 0, [0, 0, 0], "DPtrO"),
     (0b101_00110, 2, [0, 0, 0], "DLdi"),
-    (0b110_00000, 1, [0, 0, 0], "Jmp"),
-    (0b110_00001, 1, [0, 0, 0], "Jiz"),
-    (0b110_00010, 1, [0, 0, 0], "Jnz"),
-    (0b110_00011, 0, [0, 0, 0], "JmpPtr"),
-    (0b110_00100, 0, [0, 0, 0], "JizPtr"),
-    (0b110_00101, 0, [0, 0, 0], "JnzPtr"),
-    (0b110_00110, 1, [0, 0, 0], "JmpR"),
+    (0b110_00000, 1, [1, 0, 0], "Jmp"),
+    (0b110_00001, 1, [1, 0, 0], "Jiz"),
+    (0b110_00010, 1, [1, 0, 0], "Jnz"),
+    (0b110_00011, 1, [0, 0, 0], "JmpR"),
+    (0b110_00100, 1, [0, 0, 0], "JizR"),
+    (0b110_00101, 1, [0, 0, 0], "JnzR"),
     (0b111_00000, 0, [0, 0, 0], "Pop"),
     (0b111_00001, 0, [0, 0, 0], "TopL"),
     (0b111_00010, 0, [0, 0, 0], "TopR"),
     (0b111_00011, 0, [0, 0, 0], "PshO"),
+    (0b111_00100, 1, [1, 0, 0],	"PshCon"),
 ];
 static REGISTERS: &[&str] = &[
     "rda",
@@ -187,7 +191,7 @@ fn generate_headers(script: &Vec<Vec<String>>, page: usize) -> Vec<(String, usiz
 }
 
 fn main() {
-    let script = std::fs::read_to_string("../scripts/program_file.mca").unwrap();
+    let script = std::fs::read_to_string("scripts/test_program.mca").unwrap();
     let mut script = script
         .lines()
         .map(|line| line.split(" ").collect::<Vec<&str>>())
@@ -218,11 +222,178 @@ fn main() {
         headers.append(&mut generate_headers(&mut script.0, page));
     }
 
+    let mut program_bytes = vec![];
     for script_index in 0..pages.len() {
         let bytes = compile_script(&mut pages, &headers, script_index);
         println!("{}", bytes.iter().enumerate()
             .map(|(index, byte)| format!("{:>3}: {}\n\n", index, format!("{:032b}", byte)
                 .chars().map(|c| format!(" {} ", c)).collect::<String>()
             )).collect::<String>());
+        program_bytes.push(bytes);
+    }
+
+    // running the emulator
+    run_emmulator(program_bytes);
+}
+
+fn run_emmulator(program_bytes: Vec<Vec<u32>>) {
+    // memory
+    let mut registers = [0u8; 256];
+    let mut ram = [0u8; 256];
+    let mut stack = [0u8; 64];
+    let mut disc = [0u8; 256];
+
+    // dedicated registers
+    let mut program_counter = 0u16;
+    let mut alu_left  = 0u8;
+    let mut alu_right = 0u8;
+    let mut alu_out   = 0u8;
+    let mut pointer_reg = 0u8;
+    let mut overflow_flag = false;
+    let mut condition_flag = false;
+    let mut next_page_reg = 0u8;
+    let mut x_coord_reg = 0u8;
+    let mut y_coord_reg = 0u8;
+    let mut color_reg = 0u8;
+
+    let mut cycle = 0;  // just for debug stuff ig
+    let time_start = std::time::Instant::now();
+
+    loop {
+        cycle += 1;
+
+        let instruction = program_bytes[(program_counter >> 8) as usize][(program_counter & 0xFF) as usize];
+        let op_code = (instruction >> 24) as u8;
+        let reg_or_add = ((instruction >> 16) & 0xFF) as u8;
+        let immediate = ((instruction >> 8) & 0xFF) as u8;
+        let immediate_2 = (instruction & 0xFF) as u8;
+        let mut jumped = false;
+        match op_code {
+            0b000_00001 => { x_coord_reg = alu_out; },  // "SetDspInX"
+            0b000_00010 => { y_coord_reg = alu_out; },  // "SetDspInY"
+            0b000_00011 => { color_reg = alu_out; },  // "SetDspInCol"
+            0b000_00100 => {  },  // "Plot"
+            0b000_00101 => { break; },  // "Kill"
+            0b000_00110 => { pointer_reg = alu_out; },  // "SetPtr"
+            0b000_00111 => { alu_left = (program_counter & 0xFF) as u8; },  // "PgcL"
+            0b000_01000 => { alu_right = (program_counter & 0xFF) as u8; },  // "PgcR"
+            0b000_01001 => {  },  // "Plt"
+            0b000_01010 => { next_page_reg = immediate_2; },  // "SetPage"
+            0b000_01011 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add); },  // "Goto"
+            0b000_01100 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add); },  // "GotoReg"
+            0b000_01101 => { next_page_reg = registers[reg_or_add as usize]; },  // "SetPageReg"
+            0b001_00000 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Add"
+            0b001_00001 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Sub"
+            0b001_00010 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Inc"
+            0b001_00011 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Dec"
+            0b001_00100 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "ThruL"
+            0b001_00101 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "ThruR"
+            0b001_00110 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "And"
+            0b001_00111 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Or"
+            0b001_01000 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "ShftL"
+            0b001_01001 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "ShftR"
+            0b001_01010 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "LdiL"
+            0b001_01011 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "LdiR"
+            0b010_00000 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Eq"
+            0b010_00001 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Less"
+            0b010_00010 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Grtr"
+            0b010_00011 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "OvrFlow"
+            0b010_00100 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "SetC"
+            0b010_00101 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "RsetC"
+            0b010_00110 => { run_alu(op_code, immediate, &mut alu_left, &mut alu_right, &mut alu_out, &mut overflow_flag, &mut condition_flag); },  // "Zero"
+            0b011_00000 => { alu_left = registers[reg_or_add as usize]; },  // "LodL"
+            0b011_00001 => { alu_right = registers[reg_or_add as usize]; },  // "LodR"
+            0b011_00010 => { registers[reg_or_add as usize] = alu_out; },  // "WrtO"
+            0b011_00011 => { alu_left = pointer_reg; },  // "PtrL"
+            0b011_00100 => { alu_right = pointer_reg; },  // "PtrR"
+            0b011_00101 => { pointer_reg = alu_out; },  // "PtrO"
+            0b011_00110 => { registers[reg_or_add as usize] = immediate; },  // "Ldi"
+            0b100_00000 => { alu_left = ram[reg_or_add as usize]; },  // "RLodL"
+            0b100_00001 => { alu_right = ram[reg_or_add as usize]; },  // "RLodR"
+            0b100_00010 => { ram[reg_or_add as usize] = alu_out; },  // "RWrtO"
+            0b100_00011 => { alu_left = ram[pointer_reg as usize]; },  // "RPtrL"
+            0b100_00100 => { alu_right = ram[pointer_reg as usize]; },  // "RPtrR"
+            0b100_00101 => { ram[pointer_reg as usize] = alu_out; },  // "RPtrO"
+            0b100_00110 => { ram[reg_or_add as usize] = immediate; },  // "RLdi"
+            0b101_00000 => { alu_left = disc[reg_or_add as usize]; },  // "DLodL"
+            0b101_00001 => { alu_right = disc[reg_or_add as usize]; },  // "DLodR"
+            0b101_00010 => { disc[reg_or_add as usize] = alu_out; },  // "DWrtO"
+            0b101_00011 => { alu_left = disc[pointer_reg as usize]; },  // "DPtrL"
+            0b101_00100 => { alu_right = disc[pointer_reg as usize]; },  // "DPtrR"
+            0b101_00101 => { disc[pointer_reg as usize] = alu_out; },  // "DPtrO"
+            0b101_00110 => { disc[reg_or_add as usize] = immediate; },  // "DLdi"
+            0b110_00000 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add) },  // "Jmp"
+            0b110_00001 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add) },  // "Jiz"
+            0b110_00010 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add) },  // "Jnz"
+            0b110_00011 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add) },  // "JmpR"
+            0b110_00100 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add) },  // "JizR"
+            0b110_00101 => { run_lu(op_code, condition_flag, next_page_reg, &mut program_counter, &registers, &mut jumped, immediate, immediate_2, reg_or_add) },  // "JnzR"
+            0b111_00000 => { for i in 0..stack.len() - 1 { stack[i] = stack[i + 1] } stack[stack.len() - 1] = 0; },  // "Pop"
+            0b111_00001 => { alu_left = stack[0]; },  // "TopL"
+            0b111_00010 => { alu_right = stack[0]; },  // "TopR"
+            0b111_00011 => { for i in (0..stack.len() - 1).rev() { stack[i + 1] = stack[i] } stack[0] = alu_out; },  // "PshO"
+            0b111_00100 => { for i in (0..stack.len() - 1).rev() { stack[i + 1] = stack[i] } stack[0] = immediate; },  // "PshCon"
+            _ => {}
+        }
+        if !jumped {
+            program_counter += 1;
+        }
+
+        //println!("PC: {}, ALU Left: {}, ALU Right: {}, ALU Out: {}, Overflow Flag: {}, Condition Flag: {}", program_counter, alu_left, alu_right, alu_out, overflow_flag, condition_flag);
+        //println!("First 10 registers: {:?}", &registers[..10]);
+        //println!("First 20 of stack: {:?}", &stack[..20]);
+        //std::thread::sleep(Duration::from_secs_f32(0.1));
+    }
+
+    let end = time_start.elapsed().as_secs_f64() / cycle as f64;
+    println!("Average Cycle Time: {} seconds, which is about {} operations per second", end, 1.0 / end);
+}
+
+fn run_alu(op_code: u8, immediate: u8, left: &mut u8, right: &mut u8, out: &mut u8, overflow_flag: &mut bool, condition_flag: &mut bool) {
+    match op_code {
+        0b001_00000 => {
+            if left.checked_add(*right).is_none() { *overflow_flag = true; }
+            *out = *left + *right;
+        },  // "Add"
+        0b001_00001 => { *out = *left - *right; },  // "Sub"
+        0b001_00010 => {
+            if left.checked_add(1).is_none() { *overflow_flag = true; }
+            *out = *left + 1;
+        },  // "Inc"
+        0b001_00011 => { *out = *left - 1; },  // "Dec"
+        0b001_00100 => { *out = *left; },  // "ThruL"
+        0b001_00101 => { *out = *right; },  // "ThruR"
+        0b001_00110 => { *out = *left & *right; },  // "And"
+        0b001_00111 => { *out = *left | *right; },  // "Or"
+        0b001_01000 => {
+            if left.checked_shl(1).is_none() { *overflow_flag = true; }
+            *out = *left << 1;
+        },  // "ShftL"
+        0b001_01001 => { *out = *left >> 1; },  // "ShftR"
+        0b001_01010 => { *left = immediate; },  // "LdiL"
+        0b001_01011 => { *right = immediate; },  // "LdiR"
+        0b010_00000 => { *condition_flag = *left == *right; },  // "Eq"
+        0b010_00001 => { *condition_flag = *left < *right; },  // "Less"
+        0b010_00010 => { *condition_flag = *left > *right; },  // "Grtr"
+        0b010_00011 => { *condition_flag = *overflow_flag; },  // "OvrFlow"
+        0b010_00100 => { *condition_flag = true; },  // "SetC"
+        0b010_00101 => { *condition_flag = false; },  // "RsetC"
+        0b010_00110 => { *condition_flag = *out == 0; },  // "Zero"
+        _ => {}
     }
 }
+
+fn run_lu(op_code: u8, condition_flag: bool, next_page_reg: u8, program_counter_reg: &mut u16, registers: &[u8; 256], jumped: &mut bool, immediate: u8, immediate_2: u8, reg_or_add: u8) {
+    match op_code {
+        0b000_01011 => { *jumped = true; *program_counter_reg = (immediate as u16 | ((immediate_2 as u16) << 8)); },  // "Goto"
+        0b000_01100 => { *jumped = true; *program_counter_reg = (registers[reg_or_add as usize]) as u16 | ((next_page_reg as u16) << 8); },  // "GotoReg"
+        0b110_00000 => { *jumped = true; *program_counter_reg = reg_or_add as u16 | ((next_page_reg as u16) << 8); },  // "Jmp"
+        0b110_00001 => { if condition_flag { *jumped = true; *program_counter_reg = immediate as u16 | ((next_page_reg as u16) << 8); } },  // "Jiz"
+        0b110_00010 => { if !condition_flag { *jumped = true; *program_counter_reg = immediate as u16 | ((next_page_reg as u16) << 8); } },  // "Jnz"
+        0b110_00011 => { *jumped = true; *program_counter_reg = (registers[reg_or_add as usize]) as u16 | ((next_page_reg as u16) << 8); },  // "JmpR"
+        0b110_00100 => { if condition_flag { *jumped = true; *program_counter_reg = (registers[reg_or_add as usize]) as u16 | ((next_page_reg as u16) << 8); } },  // "JizR"
+        0b110_00101 => { if !condition_flag { *jumped = true; *program_counter_reg = (registers[reg_or_add as usize]) as u16 | ((next_page_reg as u16) << 8); } },  // "JnzR"
+        _ => {}
+    }
+}
+
